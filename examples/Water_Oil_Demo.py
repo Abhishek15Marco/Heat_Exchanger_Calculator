75519a7cfc3efaedee2501e23ae89d42c03d87c2
Abhishek15Marco <abhishekmarco008@gmail.com>
2025-09-29 18:34:06 +0530

    Update Water_Oil_Demo.py

--- examples/Water_Oil_Demo.py
+++ examples/Water_Oil_Demo.py
@@ -1,3 +1,11 @@
+import sys, os
+
+# Add project root to Python path so it can find "src"
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
+
+from src.heat_exchanger import heat_load, lmtd, area_required
+
 import matplotlib.pyplot as plt
 # using matplotlib for plotting
 
